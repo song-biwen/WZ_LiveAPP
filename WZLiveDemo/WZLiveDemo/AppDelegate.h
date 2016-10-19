fe7223c8627a0752efb33f18445bbf9360e2d9a4
songbiwen <1099790014@qq.com>
2016-10-19 17:10:59 +0800

    +inital

---
+++ WZLiveDemo/WZLiveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZLiveDemo
+//
+//  Created by songbiwen on 2016/10/19.
+//  Copyright © 2016年 songbiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WZLiveDemo/WZLiveDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WZLiveDemo
+//
+//  Created by songbiwen on 2016/10/19.
+//  Copyright © 2016年 songbiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
